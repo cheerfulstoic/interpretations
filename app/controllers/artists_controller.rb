9c96768f56da9182772ce8e325e324a7069f05fa
Tomás Augusto Müller <tomas.am@gmail.com>
2014-07-19 11:18:01 -0300

    Refactor scaffolded controllers.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,28 +1,20 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
-  # GET /artists
-  # GET /artists.json
   def index
     @artists = Artist.all
   end
 
-  # GET /artists/1
-  # GET /artists/1.json
   def show
   end
 
-  # GET /artists/new
   def new
     @artist = Artist.new
   end
 
-  # GET /artists/1/edit
   def edit
   end
 
-  # POST /artists
-  # POST /artists.json
   def create
     @artist = Artist.new(artist_params)
 
@@ -37,8 +29,6 @@ class ArtistsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /artists/1
-  # PATCH/PUT /artists/1.json
   def update
     respond_to do |format|
       if @artist.update(artist_params)
@@ -51,8 +41,6 @@ class ArtistsController < ApplicationController
     end
   end
 
-  # DELETE /artists/1
-  # DELETE /artists/1.json
   def destroy
     @artist.destroy
     respond_to do |format|
@@ -62,13 +50,13 @@ class ArtistsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_artist
       @artist = Artist.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def artist_params
       params.require(:artist).permit(:name, :link, :photo)
     end
+
 end
