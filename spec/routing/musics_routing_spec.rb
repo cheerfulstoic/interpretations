d46900d36cbba674271e68e5cac87771878a8843
Tomás Augusto Müller <tomas.am@gmail.com>
2014-07-19 18:10:17 -0300

    Add route specs.

--- spec/routing/musics_routing_spec.rb
+++ spec/routing/musics_routing_spec.rb
@@ -1,34 +1,34 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe MusicsController, :type => :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/musics").to route_to("musics#index")
+    it 'routes to #index' do
+      expect(:get => '/musics').to route_to('musics#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/musics/new").to route_to("musics#new")
+    it 'routes to #new' do
+      expect(:get => '/artists/1/musics/new').to route_to('musics#new', :artist_id => '1')
     end
 
-    it "routes to #show" do
-      expect(:get => "/musics/1").to route_to("musics#show", :id => "1")
+    it 'routes to #show' do
+      expect(:get => '/artists/1/musics/1').to route_to('musics#show', :id => '1', :artist_id => '1')
     end
 
-    it "routes to #edit" do
-      expect(:get => "/musics/1/edit").to route_to("musics#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(:get => '/artists/1/musics/1/edit').to route_to('musics#edit', :id => '1', :artist_id => '1')
     end
 
-    it "routes to #create" do
-      expect(:post => "/musics").to route_to("musics#create")
+    it 'routes to #create' do
+      expect(:post => '/artists/1//musics').to route_to('musics#create', :artist_id => '1')
     end
 
-    it "routes to #update" do
-      expect(:put => "/musics/1").to route_to("musics#update", :id => "1")
+    it 'routes to #update' do
+      expect(:put => '/artists/1//musics/1').to route_to('musics#update', :id => '1', :artist_id => '1')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/musics/1").to route_to("musics#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(:delete => '/artists/1//musics/1').to route_to('musics#destroy', :id => '1', :artist_id => '1')
     end
 
   end
